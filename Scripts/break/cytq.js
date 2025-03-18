c7523f5a243c73696596162f557dd752ec38edcd
LIONN6 <129770311+LIONN6@users.noreply.github.com>
2025-03-18 20:01:35 +0800

    更新 cytq.js

--- Scripts/break/cytq.js
+++ Scripts/break/cytq.js
@@ -4,6 +4,8 @@
 [rewrite_local]
 # 普通版广告
 ;^https:\/\/ad\.cyapi\.cn\/v2\/req\?app_name=weather url reject-dict
+^https:\/\/biz\.cyapi\.cn\/(p\/v1\/entries|p\/v1\/trial_card\/info|v2\/product) reject-dict
+^https:\/\/starplucker\.cyapi\.cn\/v3\/(config\/cypage\/\w+\/conditions|notification\/message_center|operation\/homefeatures) reject-dict
 # 去除赏叶赏花
 ^https:\/\/wrapper\.cyapi\.cn\/v1\/activity\?app_name=weather url script-response-body https://raw.githubusercontent.com/LIONN6/Quantumult-X/main/Scripts/break/cytq.js
 # 解锁vip
@@ -11,7 +13,7 @@
 # 卫星云图 48小时预报
 ^https:\/\/wrapper\.cyapi\.cn\/v1\/(satellite|nafp\/origin_images) url script-request-header https://raw.githubusercontent.com/LIONN6/Quantumult-X/main/Scripts/break/cytq.js
 [mitm]
-hostname = biz.cyapi.cn, wrapper.cyapi.cn
+hostname = biz.cyapi.cn, wrapper.cyapi.cn, starplucker.cyapi.cn
  */
 var caixiaoyun = {},
     url = $request.url;
